481354c9b61be89c4843f1c27dd4abf29bda25b0
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-10-17 19:02:22 +0600

    some fix

--- resources/assets/script/common/custom.js
+++ resources/assets/script/common/custom.js
@@ -18,22 +18,6 @@ $(".slider").slick({
 $(".carousel").flickity({
   // options
   cellAlign: "left",
-  contain: true
+  contain: true,
+  autoPlay: true
 });
-
-// $(".navbar li.dropdown").hover(
-//   function() {
-//     $(this)
-//       .find(".dropdown-menu")
-//       .stop(true, true)
-//       .delay(100)
-//       .fadeIn(300);
-//   },
-//   function() {
-//     $(this)
-//       .find(".dropdown-menu")
-//       .stop(true, true)
-//       .delay(100)
-//       .fadeOut(300);
-//   }
-// );
